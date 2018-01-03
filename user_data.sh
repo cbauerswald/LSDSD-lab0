6ae495a10483f14adfd392f47c60a138c90ff0db
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2018-01-03 20:36:40 +0000

    add user data to the body of page

--- user_data.sh
+++ user_data.sh
@@ -9,7 +9,7 @@ cd home/ec2-user/server
 echo "const express = require('express')" > index.js
 echo "const app = express()" >> index.js
 echo "app.get('/', (req, res) => {" >> index.js
-echo "res.send('Cecelia A. AMI ID: ami-7c052e19 user script') })" >> index.js
+echo "res.send('Cecelia A. AMI ID: ami-7c052e19 <code> #!/bin/bash <br> yum install git -y <br> git clone  https://github.com/cbauerswald/LSDSD-lab0.git <br> chmod 777 LSDSD-lab0/user_data.sh <br> echo LSDSD-lab0/user_data.sh | bash </code>') })" >> index.js
 echo "app.listen(3000, () => console.log('Server running on port 3000'))" >> index.js
 echo '{
   "name": "test",
