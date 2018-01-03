aa95b29ddf7f3d6d6da44ccab620842139634342
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2018-01-03 21:12:11 +0000

    pull current version of git repo in user data

--- user_data_base.sh
+++ user_data_base.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum install git -y
 git clone  https://github.com/cbauerswald/LSDSD-lab0.git
+git stash
+git pull origin master
 chmod 777 LSDSD-lab0/user_data.sh 
 echo LSDSD-lab0/user_data.sh | bash
