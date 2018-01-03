03b01b36ad7d0fadb1e70601ee58ee389f8de400
Cecelia Auerswald <cecelia.auerswald@students.olin.edu>
2018-01-03 17:12:47 +0000

    adding user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "in the script"
+curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash -
+sudo yum -y install nodejs
+echo "***node installation complete***"
+mkdir home/ec2-user/server
+cd home/ec2-user/server
+echo "const express = require('express')" > index.js
+echo "const app = express()" >> index.js
+echo "app.get('/', (req, res) => {" >> index.js
+echo "res.send('ccb') })" >> index.js
+echo "app.listen(3000, () => console.log('Server running on port 3000'))" >> index.js
+echo '{
+  "name": "test",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}' > package.json
+npm install express --save-dev
+echo "***user script completed***"
+node index.js
